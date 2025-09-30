077bc7b544b230739a6463d30ab1d78fd13cc5cd
한규현 <tx.seol@gmail.com>
2025-09-30 13:58:04 +0900

    2

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,8 @@ export default function Home() {
       <p>API_KEY={process.env.STRIPE_SECRET_KEY}</p>
       <p>API_KEY={process.env.NEXT_PUBLIC_API_URL}</p>
       <p>API_KEY={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}</p>
+
+      <div className="p-3">끼얏호우!</div>
     </div>
   );
 }
